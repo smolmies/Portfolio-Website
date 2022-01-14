a8ec679f7b494db4f2883fd7505eec750e4cfebd
smolmies <70819166+smolmies@users.noreply.github.com>
2022-01-14 17:00:04 +0100

    Finished aboutme section and fixed js function

--- resources/javascript/index.js
+++ resources/javascript/index.js
@@ -2,8 +2,13 @@ const ffxiv = document.querySelector("#ffxiv");
 const profileMe = document.querySelector("#profile-me");
 
 function switchProfilePic (){
-    profileMe.setAttribute('src', './resources/images/profile-wol.jpg' );
-}
+    if(profileMe.getAttribute('src') === './resources/images/profile-me.jpg'){
+        profileMe.setAttribute('src', './resources/images/profile-wol.jpg' );
+        profileMe.setAttribute('alt', "A picture of my character in FF XIV");
+    } else {
+    profileMe.setAttribute('src', './resources/images/profile-me.jpg');
+    profileMe.setAttribute('alt', "A picture of me");
+}}
 
 function addProfileEventListener(){
     ffxiv.addEventListener('click', switchProfilePic);
